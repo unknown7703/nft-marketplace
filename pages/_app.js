058e08402e994d0745e9d26a1639bdf21ea3003e
Unknown-red7703 <padmajaatreya02@gmail.com>
2022-07-27 22:53:45 +0530

    Good change

--- pages/_app.js
+++ pages/_app.js
@@ -4,25 +4,25 @@ import Link from "next/link";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="border-b p-6">
-        <p className="text-4xl font-bold">Metaverse Marketplace</p>
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-6 text-pink-500">Home</a>
-          </Link>
-          <Link href="/create-item">
-            <a className="mr-6 text-pink-500">Sell Digital Asset</a>
-          </Link>
-          <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">My Digital Assests</a>
-          </Link>
-          <Link href="/creator-daskboard">
-            <a className="mr-6 text-pink-500">Creator Dashboard</a>
-          </Link>
-        </div>
-      </nav>
-      <Component {...pageProps} />
-    </div>
+    <nav className="border-b p-6 bg-blue-600">
+      <p className="text-5xl font-bold text-yellow-400">NFT-Kart</p>
+      <div className="flex mt-4 ml-6">
+        <Link href="/">
+          <a className="mr-6 text-yellow-400">Home</a>
+        </Link>
+        <Link href="/create-item">
+        <a className="mr-6 text-yellow-400">My Warrenties</a>
+        </Link>
+        <Link href="/my-assets">
+          <a className="mr-6 text-yellow-400">Seller Login</a>
+        </Link>
+        <Link href="/creator-daskboard">
+          <a className="mr-6 text-yellow-400">More</a>
+        </Link>
+      </div>
+    </nav>
+    <Component {...pageProps} />
+  </div>
   );
 }
 
