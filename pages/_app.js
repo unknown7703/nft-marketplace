0942b5d47cb9b1031d24a0e506e476ce9093e491
Unknown-red7703 <padmajaatreya02@gmail.com>
2022-07-28 11:27:54 +0530

    Good Change
    tested

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,39 @@ import Link from "next/link";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-    <nav className="border-b p-6 bg-blue-600">
-      <p className="text-5xl font-bold text-yellow-400">NFT-Kart</p>
-      <div className="flex mt-4 ml-6">
+    <div className="flex border-b p-2 bg-blue-600 ">
+      <div className=" ml-6 mr-4">
+        <Image src={Logo} alt="Picture of the author"
+          width="45px"
+          height="45px" />
+      </div>
+
+      <div className=" w-1/2 ml-6 italic">
         <Link href="/">
-          <a className="mr-6 text-yellow-400">Home</a>
-        </Link>
-        <Link href="/create-item">
-        <a className="mr-6 text-yellow-400">My Warrenties</a>
-        </Link>
-        <Link href="/my-assets">
-          <a className="mr-6 text-yellow-400">Seller Login</a>
-        </Link>
-        <Link href="/creator-daskboard">
-          <a className="mr-6 text-yellow-400">More</a>
+          <a className="text-3xl font-bold text-[#f3f4f6]">NFT-Kart</a>
         </Link>
       </div>
-    </nav>
+
+      <div className="w-1/3  flex justify-end mt-4 ml-6 ">
+        <Link href="/">
+          <a className="ml-6 text-[#facc15]">Home</a>
+        </Link>
+        <Link href="/create-item">
+          <a className="ml-6 text-[#facc15]">My Warrenties</a>
+        </Link>
+        <Link href="/my-assets">
+          <a className="ml-6 text-[#facc15]">Seller Login</a>
+        </Link>
+        <Link href="/creator-daskboard">
+          <a className="ml-6 text-[#facc15]">More</a>
+        </Link>
+      </div>
+    </div>
+    <div>
+
+    </div>
+    
+
     <Component {...pageProps} />
   </div>
   );
